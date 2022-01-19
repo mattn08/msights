98ac3b6f10af8cb657117d1b929fe290791ff670
matt <mattn08@hotmail.com>
2022-01-19 22:20:51 +0000

    Make months selectable

--- main.py
+++ main.py
@@ -1,34 +1,52 @@
+import calendar
 import datetime
-import pandas as pd
+
 import dash
+import pandas as pd
 import pandas_datareader.data as web
-import plotly.express as px
 import plotly.graph_objects as go
-from dash import html, Output, Input
 from dash import dcc
+from dash import html, Output, Input
 from plotly.subplots import make_subplots
 
 
-start = datetime.datetime(2021, 1, 2)
-end = datetime.datetime(2021, 12, 31)
-spy = web.DataReader(['sp500'], 'fred', start, end)
-btc = web.get_data_yahoo(['BTC-USD'], start, end)['Close']
-both = pd.concat([spy, btc], axis=1).dropna(how='any', axis=0)
+def generate_start_dates(start_date, end_date):
+    return [{'label': calendar.month_name[d.month], 'value': d.strftime('%Y-%m-%d')}
+            for d in pd.date_range(start_date, end_date, freq='1M')-pd.offsets.MonthBegin(1)]
+
+
+def generate_end_dates(start_date, end_date):
+    return [{'label': calendar.month_name[d.month], 'value': d.strftime('%Y-%m-%d')}
+            for d in pd.date_range(start_date, end_date, freq='1M')]
+
 
 app = dash.Dash(__name__)
 
-fig = px.line(both, y=both.columns)
 
-fig2 = make_subplots(specs=[[{"secondary_y": True}]])
-fig2.add_trace(
-    go.Scatter(x=spy['sp500'].keys(), y=spy['sp500'].values, name="yaxis data"),
-    secondary_y=False,
+@app.callback(
+    Output(component_id='series-graph', component_property='figure'),
+    Output(component_id='series-correlation', component_property='children'),
+    Input(component_id='start-dropdown', component_property='value'),
+    Input(component_id='end-dropdown', component_property='value')
 )
-fig2.add_trace(
-    go.Scatter(x=btc['BTC-USD'].keys(), y=btc['BTC-USD'].values, name="yaxis data"),
-    secondary_y=True,
-)
+def generate_chart(start_date, end_date):
+    spy = web.DataReader(['sp500'], 'fred', start_date, end_date).dropna()
+    btc = web.get_data_yahoo(['BTC-USD'], start_date, end_date)['Close']
+    figure = make_subplots(specs=[[{"secondary_y": True}]])
+    figure.add_trace(
+        go.Scatter(x=spy['sp500'].keys(), y=spy['sp500'].values, name="S&P500"),
+        secondary_y=False,
+    )
+    figure.add_trace(
+        go.Scatter(x=btc['BTC-USD'].keys(), y=btc['BTC-USD'].values, name="BTCUSD"),
+        secondary_y=True,
+    )
+    return figure, "{:.5f}".format(spy.corrwith(btc['BTC-USD'])[0])
 
+
+start = datetime.datetime(2021, 1, 1)
+end = datetime.datetime(2021, 12, 31)
+# fig = generate_chart(start, end)
 
 app.layout = html.Div(children=[
     html.H1(children='Market Correlation Sheet'),
@@ -37,36 +55,52 @@ app.layout = html.Div(children=[
         Dash: Pick two securities and time period
     '''),
 
-    html.Div([
-       "Input: ",
-       dcc.Input(id='my-input', value='initial value', type='text')
+    html.Div(children=[
+        dcc.Dropdown(
+            id='start-dropdown',
+            options=generate_start_dates(start.strftime('%Y-%m-%d'), end.strftime('%Y-%m-%d')),
+            value=start.strftime('%Y-%m-%d')
+        ),
+
+        dcc.Dropdown(
+            id='end-dropdown',
+            options=generate_end_dates(start.strftime('%Y-%m-%d'), end.strftime('%Y-%m-%d')),
+            value=end.strftime('%Y-%m-%d')
+        ),
     ]),
+
+    # dcc.Dropdown(
+    #     options=[
+    #         {'label': 'S&P 500', 'value': 'spy'},
+    #         {'label': 'Bitcoin', 'value': 'BTC-USD'},
+    #     ],
+    #     value=['spy'],
+    # ),
+
+    dcc.Graph(id='series-graph'),
 
     html.Br(),
 
-    html.Div(id='my-output'),
-
-    dcc.Dropdown(
-        options=[
-            {'label': 'S&P 500', 'value': 'spy'},
-            {'label': 'Bitcoin', 'value': 'BTC-USD'},
-        ],
-        value=['spy'],
-    ),
-
-    dcc.Graph(
-        id='example-graph',
-        figure=fig2
-    )
+    html.Label('Correlation'),
+    html.Div(id='series-correlation'),
 ])
 
 
-@app.callback(
-    Output(component_id='my-output', component_property='children'),
-    Input(component_id='my-input', component_property='value')
-)
-def update_output_div(input_value):
-    return 'Output: {}'.format(input_value)
+# @app.callback(
+#     Output(component_id='series-correlation', component_property='children'),
+#     Input(component_id='start-dropdown', component_property='value')
+# )
+# def update_output_div(input_value):
+#     return "{:.5f}".format(spy.corrwith(btc['BTC-USD'])[0])
+
+
+# @app.callback(
+#     Output(component_id='series-correlation', component_property='value'),
+#     Input(component_id='my-input', component_property='value')
+# )
+# def calc_correlation():
+#     print(type(spy))
+#     return spy.corrwith(btc['BTC-USD'])[0]
 
 
 if __name__ == '__main__':
